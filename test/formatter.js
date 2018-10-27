3ac4b4c0f1e1cdbe681b3e0211285a36b59003fd
Nicolas <nicolas.lagoutte@connected-ventures.com>
2018-10-27 07:50:54 +0100

    Force format files

--- test/formatter.js
+++ test/formatter.js
@@ -1,62 +1,76 @@
-require('chai').should()
+require("chai").should()
 const chalk = require("chalk")
 
-const formatter = require('../lib/formatter')
+const formatter = require("../lib/formatter")
 
-describe('formatter', () => {
-  it('is a function', () => {
-    formatter.should.be.an('function')
+describe("formatter", () => {
+  it("is a function", () => {
+    formatter.should.be.an("function")
   })
 
-  it('supports empty results', () => {
+  it("supports empty results", () => {
     // eslint-disable-next-line no-unused-expressions
-    (() => formatter([])).should.not.throw
+    ;(() => formatter([])).should.not.throw
   })
 
-  it('should return a string', () => {
+  it("should return a string", () => {
     // eslint-disable-next-line no-unused-expressions
-    formatter([]).should.be.a('string').that.is.empty
+    formatter([]).should.be.a("string").that.is.empty
   })
 
-  it('should ignore non boyscout rules', () => {
-    const result = formatter([ {messages: [ { ruleId: 'not-a-boyscout'}]} ])
+  it("should ignore non boyscout rules", () => {
+    const result = formatter([{ messages: [{ ruleId: "not-a-boyscout" }] }])
     // eslint-disable-next-line no-unused-expressions
-    result.should.be.a('string').that.is.empty
+    result.should.be.a("string").that.is.empty
   })
 
-  it('should report a boyscout rule', () => {
-    const result = formatter([ {messages: [ { ruleId: 'boyscout/simple-test', line: 1, column: 2 }], filePath: 'test.js'} ])
+  it("should report a boyscout rule", () => {
+    const result = formatter([
+      {
+        messages: [{ ruleId: "boyscout/simple-test", line: 1, column: 2 }],
+        filePath: "test.js"
+      }
+    ])
     // eslint-disable-next-line no-unused-expressions
-    result.should.be.a('string').that.is.equal(
-`${chalk.bold.underline.white('boyscout/simple-test')}
-\t${chalk.blue('A test rule')}
-${chalk.yellow('\ttest.js:1:2')}
+    result.should.be.a("string").that.is.equal(
+      `${chalk.bold.underline.white("boyscout/simple-test")}
+\t${chalk.blue("A test rule")}
+${chalk.yellow("\ttest.js:1:2")}
 
 `
     )
   })
 
   it("supports rules that don't have any description", () => {
-    const result = formatter([ {messages: [ { ruleId: 'boyscout/nodesc-test', line: 1, column: 2 }], filePath: 'test.js'} ])
+    const result = formatter([
+      {
+        messages: [{ ruleId: "boyscout/nodesc-test", line: 1, column: 2 }],
+        filePath: "test.js"
+      }
+    ])
     // eslint-disable-next-line no-unused-expressions
-    result.should.be.a('string').that.is.equal(
-`${chalk.bold.underline.white('boyscout/nodesc-test')}
-${chalk.yellow('\ttest.js:1:2')}
+    result.should.be.a("string").that.is.equal(
+      `${chalk.bold.underline.white("boyscout/nodesc-test")}
+${chalk.yellow("\ttest.js:1:2")}
 
 `
     )
   })
-  
+
   it("supports rules that don't have any meta", () => {
-    const result = formatter([ {messages: [ { ruleId: 'boyscout/nometa-test', line: 1, column: 2 }], filePath: 'test.js'} ])
+    const result = formatter([
+      {
+        messages: [{ ruleId: "boyscout/nometa-test", line: 1, column: 2 }],
+        filePath: "test.js"
+      }
+    ])
     // eslint-disable-next-line no-unused-expressions
-    result.should.be.a('string').that.is.equal(
-`${chalk.bold.underline.white('boyscout/nometa-test')}
-${chalk.yellow('\ttest.js:1:2')}
+    result.should.be.a("string").that.is.equal(
+      `${chalk.bold.underline.white("boyscout/nometa-test")}
+${chalk.yellow("\ttest.js:1:2")}
 
 `
     )
   })
   // TODO test it survives rules that don't define optional meta/docs
-
 })
