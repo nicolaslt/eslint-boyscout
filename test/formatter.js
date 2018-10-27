ce83c37b818e04173f75639076bb71e1e176ae8b
Nicolas <nicolas.lagoutte@connected-ventures.com>
2018-10-27 06:45:40 +0100

    start testing for real

--- test/formatter.js
+++ test/formatter.js
@@ -1,4 +1,5 @@
 require('chai').should()
+const chalk = require("chalk")
 
 const formatter = require('../lib/formatter')
 
@@ -16,5 +17,25 @@ describe('formatter', () => {
     // eslint-disable-next-line no-unused-expressions
     formatter([]).should.be.a('string').that.is.empty
   })
+
+  it('should ignore non boyscout rules', () => {
+    const result = formatter([ {messages: [ { ruleId: 'not-a-boyscout'}]} ])
+    // eslint-disable-next-line no-unused-expressions
+    result.should.be.a('string').that.is.empty
+  })
+
+  it('should report a boyscout rule', () => {
+    const result = formatter([ {messages: [ { ruleId: 'boyscout/simple-test', line: 1, column: 2 }], filePath: 'test.js'} ])
+    // eslint-disable-next-line no-unused-expressions
+    result.should.be.a('string').that.is.equal(
+`${chalk.bold.underline.white('boyscout/simple-test')}
+\t${chalk.blue('A test rule')}
+${chalk.yellow('\ttest.js:1:2')}
+
+`
+    )
+  })
+  
+  // TODO test it survives rules that don't define optional meta/docs
 
 })
